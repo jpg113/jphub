31e40fb6d07ab46833e6cefd25ab31e9983fdc35
jeanpierre gergy <gergyjeanpierre1@gmail.com>
2024-12-10 11:46:34 +0200

    changes

--- express.js
+++ express.js
@@ -1,22 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const app = express();
 const XLSX = require('xlsx');
-const PORT=process.env.PORT || 3030
+const PORT = process.env.PORT || 3030;
+
+
+app.use(cors());
+
 app.get('/students', function (req, res) {
-    
-const filePath = './info/data.xlsx';
+    const filePath = './info/data.xlsx';
+    const workbook = XLSX.readFile(filePath);
+    const sheetName = "family"; // workbook.SheetNames[0];
+    const sheet = workbook.Sheets[sheetName];
+    const data = XLSX.utils.sheet_to_json(sheet);
+
+    res.send(data);
+});
 
 
-const workbook = XLSX.readFile(filePath);
-
-
-const sheetName ="family";//workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
-
-
-const data = XLSX.utils.sheet_to_json(sheet);
-
-  res.send(data)
-})
-
-app.listen(PORT)
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 fetch('https://jphub.onrender.com/students')
-    .then(response => response.json ())
-    .then(json   => {
-json.forEach(id=> { 
- let paraghraph=document.createElement('p')
-  paraghraph.innerHTML=id
-  document.body.appendChild(paraghraph)
-let h=document.createElement('h6')
-h.innerHTML=id.name/document.body.appendChild(h)
-});
-
-    })
-
-  
+  .then(response => response.json())
+  .then(json => {
+    json.forEach(student => {
+      let paragraph = document.createElement('p');
+      paragraph.innerHTML = ` ID: ${student['id ']} - Name: ${student.name} `; 
+      document.body.appendChild(paragraph);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
