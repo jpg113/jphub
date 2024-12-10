ad3e31ac7c341189b146bdf54a18970d36ad52b2
jeanpierre gergy <gergyjeanpierre1@gmail.com>
2024-12-10 11:59:49 +0200

    c5

--- express.js
+++ express.js
@@ -4,8 +4,10 @@ const app = express();
 const XLSX = require('xlsx');
 const PORT = process.env.PORT || 3030;
 
-
-app.use(cors());
+// Enable CORS for the specific origin
+app.use(cors({
+  origin: 'http://127.0.0.1:5500' // Allow requests only from this origin
+}));
 
 app.get('/students', function (req, res) {
     const filePath = './info/data.xlsx';
@@ -14,7 +16,10 @@ app.get('/students', function (req, res) {
     const sheet = workbook.Sheets[sheetName];
     const data = XLSX.utils.sheet_to_json(sheet);
 
-    res.send(data);
+    res.send(data); // Send the parsed data as a response
 });
 
-
+// Start the server
+app.listen(PORT, () => {
+    console.log(` Server running on port ${PORT} `);
+});
