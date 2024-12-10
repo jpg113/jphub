3ed1868891c68b253f408299658c062f32349462
jeanpierre gergy <gergyjeanpierre1@gmail.com>
2024-12-10 11:04:57 +0200

    c4

--- express.js
+++ express.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const XLSX = require('xlsx');
-
+const PORT=process.env.PORT || 3030
 app.get('/students', function (req, res) {
     
 const filePath = './info/data.xlsx';
@@ -19,4 +19,4 @@ const data = XLSX.utils.sheet_to_json(sheet);
   res.send(data)
 })
 
-app.listen(3000)
+app.listen(PORT)
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-
 fetch('https://jphub.onrender.com/students')
-  .then(response => response.json())
-  .then(json => {
-json.forEach(name => {
-let table=document.createElement('table')
-
+    .then(response => response.json ())
+    .then(json   => {
+json.forEach(id=> { 
+ let paraghraph=document.createElement('p')
+  paraghraph.innerHTML=id
+  document.body.appendChild(paraghraph)
+let h=document.createElement('h6')
+h.innerHTML=id.name/document.body.appendChild(h)
 });
 
+    })
 
-
-
-
-
-  })
+  
